456f11fd79000e1195d1e35193d6cfa461944b4b
Des1r3 <115853355+Des1r3@users.noreply.github.com>
2023-07-23 11:59:15 +0800

    Delete xshrine_clinet directory

--- xshrine_clinet/port.py
+++
@@ -1,6 +0,0 @@
-import socket
-def get_open_port():
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        s.bind(('localhost', 0))
-        s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        return s.getsockname()[1]
